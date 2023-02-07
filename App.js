753e580fac9c697e5d05a346b865cd175e8a2e46
Dylan Wiafe <36554065+dylanwiafe@users.noreply.github.com>
2023-02-07 13:39:21 -0500

    latest changes

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, Image } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import ActivityScreen from "./screens/ActivityScreen"
+import TaskTileComplete from './components/TaskTileComplete';
+import TaskTileDue from './components/TaskTileDue';
+
+function HomeScreen(){
+  return (
+    <ScrollView contentContainerStyle={styles.homeContainer}>
+      <TaskTileDue/>
+      <TaskTileComplete/>
+      <TaskTileComplete/>
+      <TaskTileComplete/>
+    </ScrollView>
+    
+  )
+}
+
+// function ActivityScreen(){
+//   return (
+//     <View>
+//       <Text>You are viewing Activities</Text>
+//     </View>
+//   )
+// }
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs(){
+  return (
+     <Tab.Navigator>
+      <Tab.Screen 
+      name="Home" 
+      component={HomeScreen}  
+      options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: () => (
+            <Image style={styles.dateIcon} source={require("./assets/home-icon.png")}/>
+            )}}/>
+      <Tab.Screen 
+      name="Activity" 
+      component={ActivityScreen} 
+      options={{
+        tabBarLabel: 'Activity',
+        tabBarIcon: () => (
+          <Image style={styles.dateIcon} source={require("./assets/activity-icon.png")}/>
+          )}}/>
+    </Tab.Navigator>
+  )};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+return(
+  <NavigationContainer>
+    <MyTabs/>
+  </NavigationContainer>
+)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  homeContainer: {
+    backgroundColor: '#5D69481A',
+    flexDirection: 'column',
+justifyContent: 'space-between'
+},
 });
---
+++ components/TaskTileDue.js
@@ -0,0 +1,86 @@
+import {  StyleSheet,Text, View, Image } from 'react-native';
+
+
+  
+  const TaskTile = ()=>{
+    return (
+        <View style={styles.tileContainer}>
+        <View style={styles.taskTile}>
+          <View style={styles.taskRow}>
+            <View style={styles.taskTileColText}>
+              <Text style={styles.taskInnerText}>Unit 4 Part 1</Text>
+              <View style={styles.taskDate}>
+              <Image style={styles.dateIcon} source={require("../assets/date-icon.png")}/>
+                <Text style={styles.taskInnerTextSub}>November 15th, 2022</Text>
+              </View>
+            </View>
+        
+        </View>
+      </View>
+      </View>
+    )
+  }
+
+  const styles = StyleSheet.create({
+    tileContainer: {
+        flexDirection: "row",
+          justifyContent: "center",
+          marginTop: 10,
+          marginBottom:  140
+
+      },
+      taskTile: {
+        width: 169,
+        height: 124,
+        borderRadius: 10,
+        backgroundColor: '#FFF',
+        position: "absolute",
+        boxShadow: "rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px",
+        borderLeftWidth: 5,
+        borderLeftColor: "red"
+    }, 
+     
+        taskInnerText: {
+        // fontFamily: 'Inter',
+        fontStyle: "normal",
+        fontWeight: 'bold',
+        fontSize: 19,
+        lineHeight: 23,
+        marginLeft: 20,
+        marginTop: 20,
+      },
+      taskInnerTextSub: {
+        fontFamily: 'Inter',
+        fontStyle: "normal",
+        fontWeight: 'bold',
+        fontSize: 11,
+        lineHeight: 23,
+        marginLeft: 5,
+        // marginBottom: 15,
+        color: "rgba(51, 51, 51, 0.75)"
+      },
+      taskTileColText:{
+        flexDirection: 'column',
+      },
+      dateIcon:{
+        marginTop: 5
+      },
+      taskDate:{
+        marginLeft: 20,
+        flexDirection: "row"
+      },
+      taskTileColIcon:{
+        flexDirection: 'column',
+        flex: 1,
+        paddingTop:30,
+        paddingLeft:100,
+      },
+      taskRow:{
+        flexDirection: "row",
+        flex: 1
+    
+      }
+    })
+  export default TaskTile
+  
+ 
